eae029f2e93cb6a5f1d607d76f0c0e02fe483f63
Ann02 <78047397+Ann02@users.noreply.github.com>
2021-05-05 00:08:21 +0300

    Update task5.js

--- task5.js
+++ task5.js
@@ -4,6 +4,27 @@ let regExp2 = /-\d{4}/g;
 let regExp3 = /[^\d-]/;
 
 function checkCard(str) {
-  alert(regExp1.test(str) && regExp2.test(str) && regExp3.test(str));
+  if (regExp3.test(str)) {
+    alert("Not correct");
+    return;
+  }
+
+  let arr = str.match(regExp1);
+  let size;
+  if (arr == null) {
+    size = 0;
+  } else size = arr.length;
+
+  let arr2 = str.match(regExp2);
+  let size2;
+  if (arr2 == null) {
+    size2 = 0;
+  } else size2 = arr2.length;
+
+  if (size == 3 && size2 == 3) {
+    alert("Correct");
+  } else {
+    alert("Not correct");
+  }
 }
 checkCard(card);
